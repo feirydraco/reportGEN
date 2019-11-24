63b019328a8e87226afe458a9cd8f56091c10c85
feirydraco <vorayash97@gmail.com>
2019-11-24 20:02:59 +0530

    full

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
-from flask import Flask, render_template, make_response
-from pylatex import *
+from flask import Flask, render_template, make_response, redirect, request
+import os
+import json
+from parser.GEN import Report
+
 
 app = Flask(__name__)
+json_file = json.load(open("./parser/report.json", "r+"))
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+
+
 
 @app.route("/")
-def home():
-    return render_template("base.html")
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 @app.route("/generate")
-def compile():
-    return render_template("base.html")
+def home():
+    global json_file
+    os.chdir('parser')
+    os.system("python3 GEN.py >/dev/null 2>&1")
+    os.chdir('..')
+    json_file = json.load(open("./parser/report.json", "r+"))
+    return render_template("content.html", data=json_file)
 
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
+@app.route("/content", methods=["GET","POST"])
+def editCoverPage():
+    if request.method == "POST":
+        print(request.form.to_dict())
+        json_file['title']['subject'] = request.form.to_dict()
+        json.dump(json_file, open("./parser/report.json", "w+"))
+        
+        return render_template("content.html", data=json_file)
     
+    return render_template("content.html", data=json_file)
+
 if __name__ == "__main__":
     app.run(debug=True)
--- parser/GEN.py
+++ parser/GEN.py
@@ -1,22 +1,29 @@
 import json
 import os
-import re
 
 
 class Report():
+
     def __init__(self, filename, subject, keywords):
+
         self.PATH = os.getcwd()
         self.name = filename
-
+        
         self.authors = "{latexGEN}"
-
-        self.titlepath = os.path.join(
-            os.getcwd(), "latex_dump", "cover", "title.tex")
-        print(self.titlepath)
         self.subject = subject
         self.keywords = "{{{}}}".format(", ".join(keywords))
-        self.filepath = os.path.join(
-            self.PATH, "latex_dump", filename + ".tex")
+
+        self.title = os.path.join(
+            os.getcwd(), "latex_dump", "title.tex")
+        self.certificate = os.path.join(
+            os.getcwd(), "latex_dump", "certificate.tex")
+        self.abstract = os.path.join(os.getcwd(), "latex_dump", "abstract.tex")
+
+        self.students = []
+
+        self.filepath = os.path.join(self.PATH, "latex_dump", filename + ".tex")
+        self.data = json.load(open(os.path.join(os.getcwd(), "report.json"), "r"))
+
         with open(self.filepath, "w+") as file:
             file.write("\\documentclass[12pt, oneside]{report}")
             file.write("\n\\usepackage{color}")
@@ -27,6 +34,18 @@ class Report():
             pdfauthor={}, 
             pdfsubject={{{}}}, 
             pdfkeywords=[{}]{{hyperref}}""".format(self.authors, self.subject, self.keywords))
+        
+        if self.data['modifications']['packages'] is not None:
+            self.add_packages((self.data['modifications']['packages']))
+        if self.data['modifications']['colors'] is not None:
+            self.defineColors((self.data['modifications']['colors']))
+        self.setGeometry()
+        self.set_linespread(1.3)
+        with open(self.filepath, "a") as report:
+            config = open("./static/config.txt", "r").read()
+            report.write(config)
+        
+
 
     def addpackage(self, package):
         """
@@ -35,30 +54,29 @@ class Report():
         with open(self.filepath, "a") as file:
             file.write("\n\\usepackage{{{}}}".format(package))
 
-    def defineColor(self, colorname, RGB):
-        """
-        colorname: user-defined colorname.
-        RGB: integer tuple values in form of (R, G, B)
+    def add_packages(self, packages):
+            """
+            packages: list of packages to be set up for report. #Must be isntalled in TeX.#
+            """
 
-        #TODO: only do this if "color" package is installed.
+            print("Adding {} packages.".format(len(packages)))
+
+            for package_name in packages:
+                self.addpackage(package_name)
+                # package name can have multiple formats for adding different kinds of packages.
+
+    def defineColors(self, colors):
+        """
+        colors: {
+            colorname: user-defined colorname.
+            RGB: integer tuple values in form of (R, G, B)
+        }
         """
         with open(self.filepath, "a") as file:
-            file.write("\n\\definecolor{{{}}}{{RGB}}{{{}, {}, {}}}".format(
-                colorname, RGB[0], RGB[1], RGB[2]))
-            print("Defining color:", colorname, RGB)
-
-    def add_packages(self, packages):
-        """
-        packages: list of packages to be set up for report. #Must be isntalled in TeX.#
-        """
-
-        print("Adding {} packages.".format(len(packages)))
-
-        for package_name in packages:
-            print("Adding:", package_name, type(package_name))
-            self.addpackage(package_name)
-            # package name can have multiple formats for adding different kinds of packages.
-
+            for i in range(len(self.data['modifications']['colors'])):
+                file.write("\n\\definecolor{{{}}}{{RGB}}{{{}, {}, {}}}".format(
+                    colors[i]["colorname"], colors[i]["RGB"][0], colors[i]["RGB"][1], colors[i]["RGB"][2]))
+                    
     def setGeometry(self, lmargin=1.25, rmargin=1.0, top=0.75, bottom=0.75):
         """
         sets geometry of margins.
@@ -78,98 +96,89 @@ class Report():
         """
         with open(self.filepath, "a") as file:
             file.write("\n\\linespread{{{}}}".format(linespread))
-
-    def addTitlePage(self, students, teachers):
-        """
-        Creates the title page, expects members to be present in self.authors;
-        students: a list consisting of upto four strings for student names.
-        teachers: a list consisting of upto three strings for teacher names.
-        """
-        edit = None
-        with open("./static/titlepage.txt", "r") as titlepage:
-            with open(self.titlepath, "w+") as texfile:
-                for line in titlepage.readlines():
-                    if edit is not None:
-                        if edit == "student":
-                            texfile.write("\\begin{tabular}{l l}")
-                            for student in students['members']:
-                                print("Writing:",
-                                      student["Name"], student["USN"])
-                                texfile.write("\n\\textcolor{{blue}}{{\\textbf{{{}}}}} & \\textcolor{{blue}}{{\\hspace{{2.7cm}}\\textbf{{{}}}}}\\\ \
-".format(student["Name"], student["USN"]))
-                            edit = None
-
-                        elif edit == "teacher":
-                            texfile.write(
-                                "\\begin{tabularx}{\\linewidth}{" + "X " * len(teachers['members']) + "}")
-                            # patched to not write "&" because it messes it up.
-                            cur_t = 0
-                            for teacher in teachers['members']:
-                                print(
-                                    "Writing:", teacher["Name"], teacher["Designation"], teacher["Department"])
-                                texfile.write("\n\\textbf{{{}}}\\linebreak\\textbf{{{}}}\\linebreak\\textbf{{{}}}\\linebreak ".format(
-                                    teacher["Name"], teacher["Designation"], teacher["Department"]))
-                                if cur_t != len(teachers['members']) - 1:
-                                    texfile.write("& ")
-                                cur_t += 1
-                            edit = None
-                    else:
-                        if "\\begin{tabular}{l l}" in line:
-                            edit = "student"
-                            continue
-                        elif "\\begin{tabularx}{\\linewidth}{X X X}" in line:
-                            edit = "teacher"
-                            continue
-                        else:
-                            texfile.write(line)
+    
+    def MainContent(self, start):
         with open(self.filepath, "a") as file:
-            file.write("\n\\input{./cover/title.tex}")
-
-    def mainContent(self, state):
-        with open(self.filepath, "a") as file:
-            if state:
+            if start:
                 file.write("\n\\begin{document}")
             else:
                 file.write("\n\\end{document}")
+    
+    def replaceTag(self, pattern, text, content):
+        if text.find(pattern) != -1:
+            text = text.replace(pattern, content)
+        return text
 
-    def setBaseStyle(self):
-        """
-        Adds headers and footers as required by RNSIT, as of 2019.
-        """
-        with open(self.filepath, "a") as file:
-            file.write("".join(open("./static/config.txt", "r")))
+    def format_horizontal(self, members):
+        val = []
+        num = len(members)
+        
+        for member in members:
+            text = "\n\\textbf{{{}}}\\linebreak\\textbf{{{}}}\\linebreak\\textbf{{{}}}\\linebreak".format(member['Name'], member['Designation'], member['Department'])
+            if num > 1:
+                text += " &"
+                num -= 1
+            val.append(text)
+        return ''.join(val)
+
+    def format_vertical(self, members):
+        val = []
+        for member in members:
+            val.append("\n\\textcolor{{blue}}{{\\textbf{{{}}}}} & \\textcolor{{blue}}{{\\hspace{{2.5cm}}\\textbf{{{}}}}}\\\\".format(member['Name'], member['USN']))     
+        return ''.join(val)
+
+    def formatStudentData(self, members):
+        text = ""
+        num = len(members)
+        for member in members:
+            text += "\\textbf{{{}}} bearing USN \\textbf{{{}}}".format(member['Name'], member['USN'])
+            if num > 1:
+                if num == 2: #second last
+                    text += " and "
+                else: text += ",  "
+                num -= 1
+        return text
+
+    def addTitle(self):
+        with open("./static/titlepage.tex", "r") as title:
+            with open(self.title, "w+") as titlepage:
+                for line in title:
+                    line = self.replaceTag("<TOPIC>", line, self.data['title']['subject']['topic'])
+                    line = self.replaceTag("<TITLE>", line, self.data['title']['subject']['title'])
+                    line = self.replaceTag("<SEMESTER>", line, self.data['title']['subject']['semester'])
+                    line = self.replaceTag("<STUDENTS>", line, self.format_vertical(self.data['title']['students']['members']))
+                    line = self.replaceTag("<TEACHERS>", line, self.format_horizontal(self.data['title']['teachers']['members']))
+                    line = self.replaceTag("<NUM_TEACHERS>", line, "X " * len(self.data['title']['teachers']['members']))
+                    titlepage.write(line)
+        with open(self.filepath, "a") as report:
+            report.write("\n\\include{./title}\n")
+
+    def addCertificate(self):
+        with open("./static/certificate.tex", "r") as certificate:
+            with open(self.certificate, "w+") as certificatepage:
+                for line in certificate:
+                    line = self.replaceTag("<TITLE>", line, self.data['title']['subject']['title'])
+                    line = self.replaceTag("<SEMESTER>", line, self.data['title']['subject']['semester'])
+                    line = self.replaceTag("<GUIDES>", line, self.format_horizontal(self.data['certificate']['Guides']))
+                    line = self.replaceTag("<NUM_GUIDES>", line, "X " * len(self.data['certificate']['Guides']))
+                    line = self.replaceTag("<STUDENTS>", line, self.formatStudentData(self.data['title']['students']['members']))
+
+                    certificatepage.write(line)
+        with open(self.filepath, "a") as report:
+            report.write("\n\\include{./certificate}\n")
 
     def generate(self):
-        os.chdir("./latex_dump/")
-        print(os.getcwd())
-        print(os.listdir())
-        os.system("pdflatex {}.tex".format(self.name))
+            os.chdir("./latex_dump/")
+            os.system("pdflatex {}.tex".format(self.name))
+            # os.system("rm ./../../static/report.pdf")
+            os.system("cp -R report.pdf ./../../static/report.pdf")
 
 
 if __name__ == "__main__":
     report = Report("report", "Web", ("Scripting", "Python", "Flask"))
-
-    packages = json.load(open("./json_dump/packages.json"))['packages']
-    report.add_packages(packages)
-
-    colors = json.load(open("./json_dump/colors.json"))['colors']
-    print(colors)
-    for color in colors:
-        report.defineColor(color['colorname'], color['RGB'])
-
-    report.setGeometry()  # left, right, bot, top (inches)
-
-    report.set_linespread(1.3)
-
-    report.setBaseStyle()
-
-    report.mainContent(True)
-
-    students = json.load(open("./json_dump/titlepage.json"))['students']
-    teachers = json.load(open("./json_dump/titlepage.json"))['teachers']
-
-    report.addTitlePage(students, teachers)
-
-    report.mainContent(False)
-
+    report.MainContent(True)
+    report.addTitle()
+    report.addCertificate()
+    report.MainContent(False)
     report.generate()
+
